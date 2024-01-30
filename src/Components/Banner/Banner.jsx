ed96d5ba0b973c61c64da45358e5013bd5b00670
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-01-30 23:06:59 +0600

    added banner image & title

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,8 +1,13 @@
-
+import banner from '../../assets/images/user.png'
 const Banner = () => {
     return (
         <div>
-            
+            <div className='flex items-center justify-between lg:p-8 lg:m-12'>
+                <div className='text-left'>
+                    <h2 className='lg:text-7xl font-bold '>One Step Closer<br /> <span >To Your Dream Job</span></h2>
+                </div>
+                <img className='w-[550px]' src={banner} />
+            </div>
         </div>
     );
 };
