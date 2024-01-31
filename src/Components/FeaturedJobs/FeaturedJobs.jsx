11afb6840cc418c5754946b6efd2ca06f1510917
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-01-31 20:14:44 +0600

    added featured jobs with dynamically

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,17 +3,17 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
 
-    const [jobs, setJobs]=useState([]);
-    useEffect(()=>{
+    const [jobs, setJobs] = useState([]);
+    useEffect(() => {
         fetch('jobs.json')
-        .then(res => res.json())
-        .then(data => setJobs(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [])
     return (
         <div>
-            <h1 className="lg:text-3xl font-bold text-center">Featured Jobs : {jobs.length}</h1>
-            <p className="mt-4 text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <div>
+            <h1 className="lg:text-4xl font-bold text-center lg:mt-24">Featured Jobs</h1>
+            <p className="mt-8 text-center text-xl font-semibold">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className="grid lg:grid-cols-3 mt-24 lg:ml-24 gap-y-6">
                 {
                     jobs.map(job => <Job key={job.id} job={job}></Job>)
                 }
--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,11 +1,28 @@
 
-const Job = ({job}) => {
-    const {logo,job_title,company_name} = job;
+const Job = ({ job }) => {
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
         <div>
-            <img src= {logo} />
-            <h2>{job_title}</h2>
-            <h3>{company_name}</h3>
+            <div>
+                <div className="card card-compact w-96 border-2 p-4 lg:w-[400px] bg-rose-100">
+                    <figure><img className="lg:w-[340px] border-2" src={logo} /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-2xl font-bold mt-4">{job_title}</h2>
+                        <p className="text-3xl font-bold">{company_name}</p>
+                        <div className="flex items-center justify-between text-lg font-semibold mt-4">
+                            <h2>{remote_or_onsite}</h2>
+                            <h2>{job_type}</h2>
+                        </div>
+                        <div className="flex items-center justify-between text-lg font-semibold mt-4">
+                            <h2>{location}</h2>
+                            <h2>{salary}</h2>
+                        </div>
+                        <div className="card-actions justify-start">
+                            <button className="bg-purple-500 p-3 mt-8 text-white font-semibold rounded-xl">Show Details</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
