cb0c0c1a26a99db8921e2d612932964699325f1e
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-01-31 20:40:40 +0600

    handle show all jobs

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,11 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
 
     const [jobs, setJobs] = useState([]);
+
+    //for show all jobs
+
+    const [dataLength, setDataLength] = useState(4);
+
     useEffect(() => {
         fetch('jobs.json')
             .then(res => res.json())
@@ -15,8 +20,13 @@ const FeaturedJobs = () => {
             <p className="mt-8 text-center text-xl font-semibold">Explore thousands of job opportunities with all the information you need. Its your future</p>
             <div className="grid lg:grid-cols-3 mt-24 lg:ml-24 gap-y-6">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={dataLength === jobs.length && 'hidden'}>
+                <div className="text-center mt-8">
+                    <button onClick={() => setDataLength(jobs.length)} className="bg-red-500 text-white font-bold p-2 rounded-xl">Show All</button>
+                </div>
             </div>
         </div>
     );
